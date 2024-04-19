253618964152fd99e045383f17aff7a33aa83a22
Demid <vasilyev.demid.e@gmail.com>
2024-04-20 00:43:52 +0300

    Show uploaded data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
 import { SignedIn } from "@clerk/nextjs";
 import { SlugUploadButton } from "~/components/upload-button";
+import { db } from "~/server/db";
 
-export default function HomePage() {
+export default async function HomePage() {
+  const voices = await db.query.voices.findMany();
+  
   return (
     <main>
       <SignedIn>
         <SlugUploadButton />
+        {voices.map((voice) => JSON.stringify(voice, null, 2))}
+        <br />
       </SignedIn>
     </main>
   );
--- src/components/upload-button.tsx
+++ src/components/upload-button.tsx
@@ -1,13 +1,17 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import { UploadButton } from "utils/uploadthing";
 
 export const SlugUploadButton = () => {
+	const router = useRouter();
+	
 	return (
 		<UploadButton
 			endpoint="voiceUploader"
 			onClientUploadComplete={(res) => {
 				console.log("Files: ", res);
+				router.refresh();
 			}}
 			onUploadError={(error: Error) => {
 				console.error(`ERROR! ${error.message}`);
