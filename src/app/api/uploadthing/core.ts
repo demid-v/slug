7f3f80548061d3929bb96354d4d64a53b04e632a
Demid <vasilyev.demid.e@gmail.com>
2024-04-19 22:34:51 +0300

    Rename upload endpoint to voiceUploader

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -3,7 +3,7 @@ import { createUploadthing, type FileRouter } from "uploadthing/next";
 const f = createUploadthing();
  
 export const ourFileRouter = {
-  imageUploader: f({ audio: { maxFileSize: "4MB" } })
+  voiceUploader: f({ audio: { maxFileSize: "4MB" } })
     .onUploadComplete(async ({ file }) => {
       console.log("Upload complete.");
       console.log("file url", file.url);
--- src/components/upload-button.tsx
+++ src/components/upload-button.tsx
@@ -4,13 +4,11 @@ import { UploadButton } from "utils/uploadthing";
 
 export const SlugUploadButton = () =>
 	<UploadButton
-		endpoint="imageUploader"
+		endpoint="voiceUploader"
 		onClientUploadComplete={(res) => {
-			// Do something with the response
 			console.log("Files: ", res);
 		}}
 		onUploadError={(error: Error) => {
-			// Do something with the error.
 			console.error(`ERROR! ${error.message}`);
 		}}
 	/>
