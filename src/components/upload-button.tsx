79a52df5b28d4d1cb60d494cf59d7dd162ff648a
Demid <vasilyev.demid.e@gmail.com>
2024-04-20 13:55:11 +0300

    Add primitive recorder

--- src/components/upload-button.tsx
+++ src/components/upload-button.tsx
@@ -1,21 +1,57 @@
 "use client";
 
-import { useRouter } from "next/navigation";
-import { UploadButton } from "utils/uploadthing";
+import { useEffect, useState } from "react";
+
+let mediaRecorder: MediaRecorder | null = null;
+const chunks: Blob[] = [];
 
 export const SlugUploadButton = () => {
-	const router = useRouter();
-	
-	return (
-		<UploadButton
-			endpoint="voiceUploader"
-			onClientUploadComplete={(res) => {
-				console.log("Files: ", res);
-				router.refresh();
-			}}
-			onUploadError={(error: Error) => {
-				console.error(`ERROR! ${error.message}`);
-			}}
-		/>
-	)
+	const [isRecording, setIsRecording] = useState(false);
+	const [mediaBlobUrl, setMediaBlobUrl] = useState<string>();
+
+	useEffect(() => {
+		if (isRecording) {
+			const constraints = { audio: true };
+		
+			navigator.mediaDevices
+				.getUserMedia(constraints)
+				.then((stream) => {
+					mediaRecorder = new MediaRecorder(stream);
+					
+					mediaRecorder.ondataavailable = (e) => {
+						chunks.push(e.data);
+
+						console.log(chunks);
+						const blob = new Blob(chunks);
+						const audioURL = URL.createObjectURL(blob);
+						setMediaBlobUrl(audioURL);
+					};
+
+					mediaRecorder.start();
+
+					console.log(mediaRecorder.state);
+					console.log("recorder started");
+
+				}).catch((err) => {
+					console.error(err);
+				})
+		} else {
+			mediaRecorder?.stop();
+
+			chunks.splice(0, chunks.length);
+
+			console.log(mediaRecorder?.state);
+			console.log("recorder stopped");
+		}
+	}, [isRecording])
+
+  return (
+    <div>
+      <p>{isRecording}</p>
+      <button onClick={() => setIsRecording((state) => !state)}>
+				{isRecording ? <span>Stop</span> : <span>Start</span>} Recording
+			</button>
+      <audio src={mediaBlobUrl} controls />
+    </div>
+  );
 };
