aaf84aab262017db554be41de3d937963c3dd958
Demid <vasilyev.demid.e@gmail.com>
2024-04-21 09:41:23 +0300

    Move uploadthing to src

--- src/components/upload-button.tsx
+++ src/components/upload-button.tsx
@@ -2,7 +2,7 @@
 
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
-import { useUploadThing } from "utils/uploadthing";
+import { useUploadThing } from "~/utils/uploadthing";
 import { Button } from "./ui/button";
 
 let mediaRecorder: MediaRecorder | null = null;
