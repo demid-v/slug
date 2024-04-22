a5b6832bfd934537d02777410092045d1e7e12bf
Demid <vasilyev.demid.e@gmail.com>
2024-04-22 21:46:39 +0300

    Make Slug in top nav a link

--- src/app/_components/top-nav.tsx
+++ src/app/_components/top-nav.tsx
@@ -1,13 +1,14 @@
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import Link from "next/link";
 import { SlugIcon } from "~/components/slug-icon";
 
 export const TopNav = () => {
   return (
     <nav className="flex h-14 items-center justify-between px-4">
-      <div className="flex items-center gap-3 font-bold">
+      <Link href="/" className="flex items-center gap-3 font-bold">
         <SlugIcon width={32} height={32} />
-        <span className="cursor-default">Slug</span>
-      </div>
+        <span>Slug</span>
+      </Link>
       <SignedOut>
         <SignInButton />
       </SignedOut>
