f0ee3986dd210ad73f04df2aa6130f1f5c646d83
Demid <vasilyev.demid.e@gmail.com>
2024-04-26 01:20:01 +0300

    Adjust slug's size on main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,12 @@ import { SlugIcon } from "~/components/slug-icon";
 export default async function HomePage() {
   return (
     <>
-      <SlugIcon width="100%" height="100%" className="max-h-[512px]" />
-      <h1 className="text-5xl">
+      <SlugIcon
+        width="100%"
+        height="100%"
+        className="max-h-[450px] max-w-[450px]"
+      />
+      <h1 className="text-center text-6xl">
         <span className="font-semibold">Welcome to </span>
         <Link href="/chat" className="font-bold">
           Slug
