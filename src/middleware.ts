6ae6c3a2cd83b74decc7c462d97ccbba50b9f7e8
Demid <vasilyev.demid.e@gmail.com>
2024-05-10 13:56:00 +0300

    Protect my-chats route

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,10 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/chat(.*)"]);
+const isChatRoute = createRouteMatcher(["/chats/(.*)"]);
+const isMyChatsRoute = createRouteMatcher(["/my-chats"]);
 
 export default clerkMiddleware((auth, request) => {
-  if (isProtectedRoute(request)) auth().protect();
+  if (isChatRoute(request) || isMyChatsRoute(request)) auth().protect();
 });
 
 export const config = {
