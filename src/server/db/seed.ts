6332d1e9d9995f360d43af85781991ea6dde661d
Demid <vasilyev.demid.e@gmail.com>
2024-05-19 00:42:57 +0300

    Make seeding more controlled

--- src/server/db/seed.ts
+++ src/server/db/seed.ts
@@ -1,13 +1,12 @@
 import { utapi } from "../uploadthing";
 import { clerkClient } from "@clerk/clerk-sdk-node";
-import { faker } from "@faker-js/faker";
 import { path as ffprobePath } from "@ffprobe-installer/ffprobe";
 import { Blob } from "buffer";
 import "dotenv/config";
+import { getTableName, sql } from "drizzle-orm";
 import ffmpeg from "fluent-ffmpeg";
 import fs, { type Dirent } from "fs";
 import path from "node:path";
-import { exit } from "process";
 import { type FileEsque } from "uploadthing/types";
 import { db } from "~/server/db";
 import { chats, voices } from "~/server/db/schema";
@@ -17,18 +16,31 @@ ffmpeg.setFfprobePath(ffprobePath);
 const deleteDataFromDb = async () => {
   await db.delete(chats).where(undefined);
 
+  await db.execute(
+    sql.raw(
+      `ALTER SEQUENCE ${getTableName(chats)}_${chats.id.name}_seq RESTART WITH 1`,
+    ),
+  );
+  await db.execute(
+    sql.raw(
+      `ALTER SEQUENCE ${getTableName(voices)}_${voices.id.name}_seq RESTART WITH 1`,
+    ),
+  );
+
   const utFilesIds = (await utapi.listFiles()).files.map((file) => file.key);
   await utapi.deleteFiles(utFilesIds);
 };
 
 const seedChats = async () => {
-  const data = [];
-
-  for (let i = 0; i < 5; i++) {
-    data.push({
-      name: faker.lorem.words({ min: 1, max: 30 }).slice(0, 128),
-    });
-  }
+  const data = [
+    { name: "⛑氣象巴打" },
+    {
+      name: "Mika Paradise | Social 🔮 Gaming 👾 Chill 💬 Active ✨ Emotes & Emojis ⚡ VC & Nitro",
+    },
+    { name: "Spreen" },
+    { name: "🎥 ᴍɪʟʟɪᴏɴ ᴍᴏᴠɪᴇꜱ | ᴍᴏᴠɪᴇ ʀᴇQᴜᴇꜱᴛɪɴɢ ɢʀᴏᴜᴘ 2024" },
+    { name: "Заброшенный мир" },
+  ];
 
   return (await db.insert(chats).values(data).returning()).map(
     (chat) => chat.id,
@@ -36,12 +48,6 @@ const seedChats = async () => {
 };
 
 const seedVoices = async (chatsIds: number[]) => {
-  const mockVoicesPath = path.join(process.cwd(), "public/mock-voices");
-
-  const files = fs
-    .readdirSync(mockVoicesPath, { withFileTypes: true })
-    .filter((dirent) => dirent.isFile());
-
   const getDurationFromPath = (file: Dirent) =>
     new Promise<{
       fileName: string;
@@ -57,40 +63,71 @@ const seedVoices = async (chatsIds: number[]) => {
       });
     });
 
-  for (const file of files) {
+  const getRandom = <T>(items: T[]) =>
+    items.at(Math.floor(Math.random() * items.length));
+
+  const mockVoicesPath = path.join(process.cwd(), "public/mock-voices");
+
+  const files = fs
+    .readdirSync(mockVoicesPath, { withFileTypes: true })
+    .filter((dirent) => dirent.isFile());
+
+  const getFileData = async (file: Dirent) => {
     const fileBlob = new Blob([
       fs.readFileSync(`${file.parentPath}/${file.name}`),
     ]) as FileEsque;
     fileBlob.name = file.name;
 
-    const uploadedFile = await utapi.uploadFiles(fileBlob);
-    const duration = (await getDurationFromPath(file)).duration ?? 0;
+    return {
+      uploadedFileUrl: (await utapi.uploadFiles(fileBlob)).data?.url,
+      duration: (await getDurationFromPath(file)).duration,
+    };
+  };
 
-    const getRandom = <Item>(items: Item[]) =>
-      items.at(Math.floor(Math.random() * items.length));
+  const isPromiseFulfilled = <T>(
+    promise: PromiseSettledResult<T>,
+  ): promise is PromiseFulfilledResult<T> => promise.status === "fulfilled";
+
+  const uploadedFilesData = (
+    await Promise.allSettled(files.map(getFileData))
+  ).filter(isPromiseFulfilled);
+
+  const insertVoices = async (chatId?: number) => {
+    const { uploadedFileUrl, duration } =
+      getRandom(uploadedFilesData)?.value ?? {};
 
     const randomUser = getRandom(
       (await clerkClient.users.getUserList()).data,
     )?.id;
 
+    const randomChatId = chatId ?? getRandom(chatsIds);
+
     await db.insert(voices).values({
-      url: uploadedFile.data?.url ?? "",
-      duration,
+      url: uploadedFileUrl ?? "",
+      duration: duration ?? 0,
       userId: randomUser ?? "",
-      chatId: getRandom(chatsIds) ?? 0,
+      chatId: randomChatId ?? 0,
     });
-  }
+  };
+
+  const generateVoiceInserts = (times: number, chatId: number) =>
+    new Array(times).fill(0).map(() => insertVoices(chatId));
+
+  const insertVoicesIntoChat: Promise<void>[] = [];
+
+  insertVoicesIntoChat.push(...generateVoiceInserts(1, 2));
+  insertVoicesIntoChat.push(...generateVoiceInserts(5, 3));
+  insertVoicesIntoChat.push(...generateVoiceInserts(15, 4));
+  insertVoicesIntoChat.push(...generateVoiceInserts(50, 5));
+
+  await Promise.allSettled(insertVoicesIntoChat);
 };
 
 const seed = async () => {
-  console.log("Seed start");
+  console.log("Seed started");
 
   const chatsIds = await seedChats();
-
-  const voiceSeederPromises = new Array(3)
-    .fill(0)
-    .map(() => seedVoices(chatsIds));
-  await Promise.allSettled(voiceSeederPromises);
+  await seedVoices(chatsIds);
 
   console.log("Seed done");
 };
@@ -98,4 +135,4 @@ const seed = async () => {
 await deleteDataFromDb();
 await seed();
 
-exit(0);
+process.exit(0);
