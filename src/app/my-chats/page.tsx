cca68bc5595f22a1ab4a1ae3a6aba81bc9992c1e
Demid <vasilyev.demid.e@gmail.com>
2024-05-21 20:50:28 +0300

    Fix my chats layout

--- src/app/my-chats/page.tsx
+++ src/app/my-chats/page.tsx
@@ -1,25 +1,26 @@
 import { auth } from "@clerk/nextjs/server";
 import Link from "next/link";
 import { getMyChats } from "~/server/actions";
-import "~/styles/globals.css";
 
 const MyChatsPage = async () => {
   const { userId } = auth();
   const myChats = userId !== null ? await getMyChats(userId) : [];
 
   return (
-    <div className="flex h-full w-96 flex-col gap-8">
-      <ol className="flex flex-col gap-2">
-        {myChats.map((chat) => (
-          <li key={chat.id}>
-            <Link href={`/chat/${chat.id}`}>
-              <div className="rounded-md border border-slate-200 p-2">
-                {chat.name}
-              </div>
-            </Link>
-          </li>
-        ))}
-      </ol>
+    <div className="flex justify-center">
+      <div className="w-96">
+        <ol className="flex flex-col gap-2">
+          {myChats.map((chat) => (
+            <li key={chat.id}>
+              <Link href={`/chats/${chat.id}`}>
+                <div className="rounded-md border border-slate-200 px-3 py-2">
+                  {chat.name}
+                </div>
+              </Link>
+            </li>
+          ))}
+        </ol>
+      </div>
     </div>
   );
 };
