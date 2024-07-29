c55f98a85f47344b0240f4d873e9e488eadccdb4
Demid <vasilyev.demid.e@gmail.com>
2024-07-30 00:06:47 +0300

    Use app's env

--- src/app/api/awake-redis/route.ts
+++ src/app/api/awake-redis/route.ts
@@ -1,9 +1,10 @@
+import { env } from "~/env";
 import { ratelimitChats } from "~/server/ratelimit";
 
 export const GET = async (request: Request) => {
   const authHeader = request.headers.get("authorization");
 
-  if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+  if (authHeader !== `Bearer ${env.CRON_SECRET}`) {
     return new Response("Unauthorized", {
       status: 401,
     });
